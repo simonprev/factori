bb9f0305b50b7d33a52c992189dbd95d30efccdb
Grant McLendon <grant@7mind.de>
2022-09-07 09:05:03 -0500

    Support Ecto.Enum for Enum values (#1)
    * ✏️ correct CI psql db name
    
    * ✨ adapter/postgresql - support pulling values from Ecto.Enum fields in addition to DB enforced enums
    
    * ♻️ test/enum_test - document and removing warnings from support module name colisions

--- test/factori/enum_test.exs
+++ test/factori/enum_test.exs
@@ -6,15 +6,31 @@ defmodule Factori.EnumTest do
       Factori.TestRepo.query!("CREATE TYPE user_type AS ENUM ('admin', 'user')")
       create_table!(:users, [{:add, :type, :user_type, [null: false]}])
 
-      defmodule UserFactory do
+      defmodule DbEnumUserFactory do
         use Factori,
           repo: Factori.TestRepo,
           mappings: [Factori.Mapping.Enum]
       end
 
-      UserFactory.bootstrap()
+      DbEnumUserFactory.bootstrap()
 
-      user = UserFactory.insert("users")
+      user = DbEnumUserFactory.insert("users")
+      assert user.type in ["admin", "user"]
+    end
+
+    test "from schema ecto enum" do
+      Code.ensure_compiled!(UserEnumSchema)
+      create_table!(:users, [{:add, :type, :string, [null: false]}])
+
+      defmodule EctoEnumUserFactory do
+        use Factori,
+          repo: Factori.TestRepo,
+          mappings: [Factori.Mapping.Enum]
+      end
+
+      EctoEnumUserFactory.bootstrap()
+
+      user = EctoEnumUserFactory.insert("users")
       assert user.type in ["admin", "user"]
     end
   end
