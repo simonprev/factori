d67eb842900e7c4ee023e2a34c6341e8e1e98229
James Gough <james@peacefulprogramming.dev>
2024-08-18 16:04:59 +0100

    recreate the problem I am seeing

---
+++ test/factori/ex_money_test.exs
@@ -0,0 +1,63 @@
+defmodule Factori.ExMoneyTest do
+  use Factori.EctoCase, async: true
+
+  test "schema" do
+    create_table!(:delivery_methods, [
+      {:add, :id, :integer, [primary_key: true, null: false]},
+      {:add, :price, :money_with_currency, [null: false]}
+    ])
+
+    reference = %Ecto.Migration.Reference{
+      name: :delivery_method_id,
+      type: :bigint,
+      table: :delivery_methods
+    }
+
+    create_table!(:products, [
+      {:add, :id, :integer, [null: false]},
+      {:add, :price, :money_with_currency, [null: false]},
+      {:add, :delivery_method_id, reference, [null: false]}
+    ])
+
+    defmodule DeliveryMethodSchema do
+      use Ecto.Schema
+
+      @primary_key {:id, :string, []}
+      schema "delivery_methods" do
+        field(:price, Money.Ecto.Composite.Type)
+      end
+    end
+
+    defmodule ProductSchema do
+      use Ecto.Schema
+
+      @primary_key {:id, :string, []}
+      schema "products" do
+        field(:price, Money.Ecto.Composite.Type)
+        belongs_to(:delivery_method, DeliveryMethodSchema)
+      end
+    end
+
+    defmodule ProductFactory do
+      use Factori,
+        repo: Factori.TestRepo,
+        variants: [{:product, ProductSchema}],
+        mappings: [
+          fn
+            %{name: :id} -> 1
+            # %{name: :price} -> Money.new(:EUR, "420.69")
+            %{type: "money_with_currency"} -> Money.new(:EUR, "420.69")
+          end
+        ]
+    end
+
+    ProductFactory.bootstrap()
+
+    product = ProductFactory.insert(:product)
+    assert product.__struct__ === ProductSchema
+    assert product.id == 1
+    assert product.price == Money.new(:EUR, "420.69")
+    assert product.delivery_method_id
+    assert product.delivery_method.price == Money.new(:EUR, "420.69")
+  end
+end
